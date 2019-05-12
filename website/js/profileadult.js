bc15ae50098834e9757d559f717371d3f32b7887
Nguyễn Minh Hiếu <nmhieu.18it1@sict.udn.vn>
2019-05-12 20:36:26 +0700

    MinhHieu

---
+++ website/js/profileadult.js
@@ -0,0 +1,14 @@
+
+function showlistkid() {
+	// body...
+	var listkid = document.getElementById('listkid');
+	listkid.style.height = '280px';
+	listkid.style.width = '200px';
+	listkid.style.opacity = '1';		
+}
+function hidelistkid() {
+	var listkid = document.getElementById('listkid');
+	listkid.style.height = '0';
+	listkid.style.width = '0px';
+	listkid.style.opacity = '0';	
+}
