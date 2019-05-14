cf9eef5a11cabe7ad248cb1430ae813f97cd0088
Nguyễn Minh Hiếu <nmhieu.18it1@sict.udn.vn>
2019-05-14 13:03:12 +0700

    MinhHieu

--- website/js/test01.js
+++ website/js/test01.js
@@ -4,6 +4,7 @@ function form01() {
 	checkform = -checkform;
 	var formsi = document.getElementById('formsignin');
 	var formsu = document.getElementById('formsignup');
+	var change = document.getElementById('buttonsign');
 	// alert("Hello");
 	if (checkform == 1) {show(formsi,formsu);}
 	if (checkform == -1) {hide(formsi,formsu);}
@@ -13,6 +14,7 @@ function form01() {
 		f1.style.right = '0';
 		f2.style.opacity = '0';
 		f2.style.right = '20%';
+		change.setAttribute('value','Sign Up');
 	}
 	function hide(f1,f2) {
 		// body...
@@ -20,5 +22,6 @@ function form01() {
 		f1.style.right = '20%';
 		f2.style.opacity = '1';
 		f2.style.right = '0';
+		change.setAttribute('value','Sign In');
 	}
 };
