49ad0ca51e2562d43632ccf458abac99a9f0d745
Nguyễn Minh Hiếu <nmhieu.18it1@sict.udn.vn>
2019-05-15 17:49:01 +0700

    MinhHieu

--- website/js/index.js
+++ website/js/index.js
@@ -32,16 +32,37 @@ var nav = document.querySelector('#navguide');
 var cont = document.querySelector('#content');
 function mucdich() {
 	// body...
-	nav.innerHTML = "<h1 id='canhan'>Cá nhân</h1><h1 id='xahoi'>Xã hội</h1>";
+	nav.innerHTML = "<h1 id='canhan' onclick='intext(this)'>Cá nhân</h1><h1 id='xahoi' onclick='intext(this)'>Xã hội</h1>";
 	cont.innerHTML = "<p>Bạn đang ở phần Mục Đích...</p>"
 };
 function huongdan() {
 	// body...
-	nav.innerHTML = "<h1 id='dangnhap'>Đăng nhập</h1><h1 id='dangky'>Đăng ký</h1><h1 id='nhanqua'>Nhận quà</h1>";
+	nav.innerHTML = "<h1 id='dangnhap' onclick='intext(this)'>Đăng nhập</h1><h1 id='dangky' onclick='intext(this)'>Đăng ký</h1><h1 id='nhanqua' onclick='intext(this)'>Nhận quà</h1>";
 	cont.innerHTML = "<p>Bạn đang ở phần Hướng Dẫn...</p>"
 };
 function quatang() {
 	// body...
-	nav.innerHTML = "";
+	nav.innerHTML = "<h1 id='thubong' onclick='intext(this)'>Thú Bông</h1><h1 id='mockhoa' onclick='intext(this)'>Móc Khóa</h1><h1 id='dongho' onclick='intext(this)'>Đồng Hồ</h1><h1 id='goi' onclick='intext(this)'>Gối</h1>";
 	cont.innerHTML = "<p>Bạn đang ở phần Quà Tặng...</p>"
 };
+var mnr = -1;
+function showmenures() {
+	// body...
+	mnr = -mnr;
+	var menure = document.getElementById('menures');
+	if (mnr == 1) {
+	menure.style.opacity = '1';
+	menure.style.zIndex = '99';
+	}
+	else {
+	menure.style.opacity = '0';
+	menure.style.zIndex = '-1';
+	}
+}
+function hidemenures() {
+	// body...
+	var menure = document.getElementById('menures');
+	mnr = -1;
+	menure.style.opacity = '0';
+	menure.style.zIndex = '-1';
+}
