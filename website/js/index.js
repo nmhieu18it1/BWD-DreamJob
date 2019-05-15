88e99f16d8fce1a8b434ccad27446468000ae840
Nguyễn Minh Hiếu <nmhieu.18it1@sict.udn.vn>
2019-05-15 12:43:39 +0700

    MinhHieu

--- website/js/index.js
+++ website/js/index.js
@@ -12,3 +12,14 @@ function hidesummory() {
 	var obj = document.querySelector('#backtop p');
 	obj.style.opacity = '0';
 };
+function conf() {
+	// body...
+	var obj = document.querySelector('#inputemail #email');
+	if (obj.value == "") {
+		alert('Bạn chưa nhập email. Vui lòng nhập email để chúng tôi có thể hỗ trợ bạn.');
+	}
+	else {
+		confirm("Email của bạn là: " + obj.value + ". Cảm ơn bạn đã tin tưởng chúng tôi.");
+	}
+	
+}
