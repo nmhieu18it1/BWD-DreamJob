acc87634975f8629c0d0bae1823aa1fe3d1fcc4c
Nguyễn Minh Hiếu <nmhieu.18it1@sict.udn.vn>
2019-05-16 20:03:16 +0700

    MinhHIeu

--- website/js/signin.js
+++ website/js/signin.js
@@ -1,5 +1,7 @@
 var formsi = document.getElementById('formsignin');
 var formsu = document.getElementById('formsignup');
+var title = document.querySelector('#contenttext h1');
+var content = document.querySelector('#contenttext p');
 	// alert("Hello");
 function show() {
 	// body...
@@ -9,6 +11,8 @@ function show() {
 	formsu.style.opacity = '0';
 	formsu.style.right = '100px';
 	formsu.style.zIndex = '0';
+	title.innerHTML = 'Chào mừng bạn đến với <span>DreamJob</span>';
+	content.innerHTML = '<q>&#160 Những ước mơ không chết chừng nào bạn còn nuôi dưỡng nó bằng niềm đam mê.&#160</q><span id="tacgia">- Nick Vujicic -</span>';
 }
 function hide() {
 	// body...
@@ -18,4 +22,7 @@ function hide() {
 	formsu.style.opacity = '1';
 	formsu.style.right = '-60px';
 	formsu.style.zIndex = '999';
+	content.style.marginTop = '40px';
+	title.innerHTML = 'Một số lưu ý khi <span>Đăng Ký</span>';
+	content.innerHTML = '<span>HỌC SINH:</span> Có thể chơi game - tích điểm - đổi quà.<br><br><span>PHỤ HUYNH:</span> Có thể theo dõi trẻ em được liên kết về xu hướng - hoạt động.<br><br><span>GIÁO VIÊN:</span> Có thể tạo ra trò chơi để kiểm tra xu hướng phát triển cho học sinh.';
 }
