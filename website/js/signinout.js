889f2f75d31bd8de755fccf61f09654755ed3f70
Nguyễn Minh Hiếu <nmhieu.18it1@sict.udn.vn>
2019-05-18 14:02:43 +0700

    MinhHieu

--- website/js/signinout.js
+++ website/js/signinout.js
@@ -1,31 +1,15 @@
 
-function checksign(a) {
-	// body...
-	var obj = document.getElementById('menu3');
-	if (a== 0) {
-		obj.innerHTML = 'Đăng Nhập';
-	}
-	else if (a == 1) {
-		obj.innerHTML = 'Trang cá nhân <i class="fas fa-seedling"></i>';
-	}
-	else {
-		obj.innerHTML = 'Trang cá nhân <i class="fab fa-pagelines"></i>';
-	}
-}
 function signin() {
 	// body...
 	var obj = document.getElementById('emailsignin');
-	var objbody = document.getElementsByTagName('body');
-	alert(objbody.innerHTML);
 	if (obj.value == 'hocsinh@gmail.com') {
 		alert('Đăng nhập thành công !');
-		window.location.assign("../html/index.html");
-		objbody.setAttribute('onload','checksign(1);');
+		// window.location.assign("../html/index.html");
+		window.location.href = '../html/indexkid.html';
 	}
 	else if (obj.value == 'phuhuynh@gmail.com') {
 		alert('Đăng nhập thành công !');
-		window.location.assign("../html/index.html");
-		objbody.setAttribute('onload','checksign(2);');
+		window.location.href = '../html/indexadult.html';
 	}
 	else {
 		alert('Đăng nhập thất bại !');
